dca3c83d733f3e3abae2869ea40c7ec4e038173b
Mario Ramos <marioramosmontesinos@gmail.com>
2022-10-11 21:16:14 +0200

    TwitterClient in TwitterService's constructor

--- Infrastructure/Services/TwitterService.cs
+++ Infrastructure/Services/TwitterService.cs
@@ -5,7 +5,10 @@ namespace Infrastructure.Services;
 
 public class TwitterService: ITwitterSender
 {
-    public async Task SendTweet(string message)
+
+    private readonly TwitterClient _client;
+    
+    public TwitterService()
     {
         var apiKey = Environment.GetEnvironmentVariable("TwitterApiKey");
         if (string.IsNullOrWhiteSpace(apiKey)) throw new Exception("Twitter API key not found");
@@ -15,7 +18,11 @@ public class TwitterService: ITwitterSender
         if (string.IsNullOrWhiteSpace(accessToken)) throw new Exception("Twitter access token not found");
         var accessTokenSecret = Environment.GetEnvironmentVariable("TwitterAccessTokenSecret");
         if (string.IsNullOrWhiteSpace(accessTokenSecret)) throw new Exception("Twitter access token secret not found");
-        var userClient = new TwitterClient(apiKey, apiSecretKey, accessToken, accessTokenSecret);
-        var publishedTweet = await userClient.Tweets.PublishTweetAsync(message);
+        _client = new TwitterClient(apiKey, apiSecretKey, accessToken, accessTokenSecret);
+    }
+    
+    public async Task SendTweet(string message)
+    {
+        var publishedTweet = await _client.Tweets.PublishTweetAsync(message);
     }
 }
