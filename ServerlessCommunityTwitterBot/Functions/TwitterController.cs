9daa2e4e3e077c2145c83cc005bcf819c783aba2
Mario Ramos <marioramosmontesinos@gmail.com>
2022-10-16 01:15:36 +0200

    Update TwitterController.cs
    disable RunOnStartup

--- ServerlessCommunityTwitterBot/Functions/TwitterController.cs
+++ ServerlessCommunityTwitterBot/Functions/TwitterController.cs
@@ -21,7 +21,11 @@ public class TwitterController
     }
 
     [FunctionName("PublishTweet")]
-    public async Task PublishTweet([TimerTrigger("0 0 */12 * * *", RunOnStartup = true)] TimerInfo myTimer, ILogger log)
+    public async Task PublishTweet([TimerTrigger("0 0 */12 * * *", 
+#if DEBUG
+        RunOnStartup = true
+#endif
+        )] TimerInfo myTimer, ILogger log)
     {
         log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
         var response = await _mediator.Send(new GetNextSentenceQuery());
@@ -32,5 +36,6 @@ public class TwitterController
             LastUse = DateTime.Now,
             TimesUsed = response.TimesUsed + 1
         });
+        log.LogInformation($"Tweet sent: {response.Text}");
     }
 }
